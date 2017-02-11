8eb35a3ff3d232a4e2d2d0b26e73c0532bfb4a0e
Armando <apiconz@gmail.com>
2017-02-11 16:37:49 -0500

    Se agregó un ID al TextView y se obtuvo la referencia en el activity

--- app/src/main/java/pe/devpicon/android/myfirstkotlinapplication/MainActivity.kt
+++ app/src/main/java/pe/devpicon/android/myfirstkotlinapplication/MainActivity.kt
@@ -2,11 +2,15 @@ package pe.devpicon.android.myfirstkotlinapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val txtHelloWold = findViewById(R.id.txt_hello_world) as TextView
+        txtHelloWold.text = "Hola Armando"
     }
 }
